889ec90bc011c28370b18607b20c10cc9f88c28a
Rafael Belokurows <rafabelokurows@gmail.com>
2022-12-21 22:05:05 +0000

    Add files via upload

--- ruas porto 2.R
+++ ruas porto 2.R
@@ -1,53 +1,62 @@
+library(sf)
 library(stringr)
 library(tidywikidatar)
 library(tidyverse)
-if(!require("devtools")) install.packages("devtools")
-devtools::install_github("meirelesff/genderBR")
+# if(!require("devtools")) install.packages("devtools")
+# devtools::install_github("meirelesff/genderBR")
 library(genderBR)
+library(osmdata)
 tw_enable_cache()
 tw_set_cache_folder(path = fs::path(fs::path_home_r(), "R", "tw_data"))
 tw_set_language(language = "en")
 tw_create_cache_folder(ask = FALSE)
 
+city_coords <- matrix(data = c(-8.71902,41.10626,-8.54393,41.20758),
+                      nrow = 2, ncol = 2)
+
+limits <-  c(city_coords[1,1], city_coords[2,1],
+             city_coords[1,2], city_coords[2,2])
+
+
 streets <- opq(bbox = limits)  %>%
   add_osm_feature(key = "highway") %>%
   osmdata_sf()
 streets
-available_features()
-osmdata::available_tags("highway")
-
-unique(streets$osm_lines$name) %>% sort() %>% as.data.frame() %>% 
-  rename(rua=1) %>% 
-  mutate(nome = case_when(str_starts(rua,"NÃ³ ")~str_sub(rua,4),
+#available_features()
+#osmdata::available_tags("highway")
+unique(streets$osm_lines$name)
+unique(streets$osm_lines$name) %>% iconv("UTF-8","latin1") %>% sort() %>% as.data.frame() %>%
+  rename(rua=1) %>%
+  mutate(nome = case_when(str_starts(rua,"Nó ")~str_sub(rua,4),
                              str_starts(rua,"Rua |Via ")~str_sub(rua,5),
                              str_starts(rua,"Muro |Cais |Beco |Vila ")~str_sub(rua,6),
-                             str_starts(rua,"PÃ¡tio |Campo |PraÃ§a |Largo |Viela |Rampa |TÃºnel |Ponte ")~str_sub(rua,7),
+                             str_starts(rua,"Pátio |Campo |Praça |Largo |Viela |Rampa |Túnel |Ponte ")~str_sub(rua,7),
                              str_starts(rua,"Jardim |Rossio |Escada |Parque |Bairro |Quinta |Vereda ")~str_sub(rua,8),
-                             str_starts(rua,"Ribeira |Recanto |Estrada |Avenida |Viaduto |Praceta |CalÃ§ada |Alameda |Rotunda |Escadas |Passeio |Ladeira |Caminho ")~str_sub(rua,9),
+                             str_starts(rua,"Ribeira |Recanto |Estrada |Avenida |Viaduto |Praceta |Calçada |Alameda |Rotunda |Escadas |Passeio |Ladeira |Caminho ")~str_sub(rua,9),
                              str_starts(rua,"Travessa |Ciclovia |Tribunal ")~str_sub(rua,10),
                              str_starts(rua,"Miradouro |Escadaria ")~str_sub(rua,11))
-         
-         ) %>% 
+
+         ) %>%
   mutate(teste=str_starts(nome,"^de\\s|^da\\s|^ da\\s|^do\\s"),
          clean=case_when(str_starts(nome,"^de\\s|^ da\\s|^da\\s|^do\\s")~str_sub(nome,4),
                          str_starts(nome,"^das\\s|^dos\\s")~str_sub(nome,5),
                          TRUE~nome)
-         ) %>% 
+         ) %>%
   filter(!is.na(clean)) %>%
   mutate(genero =get_gender(clean)) -> ruas
 
 ruas %>% View()
 ruas2 %>% count(genero,sexoWiki)
-saveRDS(ruas,"ruas.rds")
+saveRDS(ruas,"ruas2112.rds")
 ruas2 = ruas %>%
   mutate(sexoWiki = case_when(!is.na(clean)~tw_get_label(tw_get_property(tw_search(search = clean,limit = 1)$id,p="P21")$value),
                               TRUE~NA_character_))
 
-saveRDS(ruas2,"ruas2.rds")  
-ruas2 %>% 
+saveRDS(ruas2,"ruas2.rds")
+ruas2 %>%
   mutate(sexoFinal = case_when(tolower(genero)==sexoWiki~genero,
                                !is.na(genero)& is.na(sexoWiki)~genero,
-                               is.na(genero)&!is.na(sexoWiki)~stringr::str_to_title(sexoWiki))) %>% 
+                               is.na(genero)&!is.na(sexoWiki)~stringr::str_to_title(sexoWiki))) %>%
   clipr::write_clip()
 
 codigos = readRDS("C:\\Users\\rafae\\Downloads\\20221205 codigos ajustados.rds")
@@ -59,19 +68,28 @@ ruas2 %>% filter(!clean %in% codigos2$nome_arteria) %>% clipr::write_clip()
 ruas2 %>% left_join(codigos2,by=c("clean"="nome_arteria")) %>% View()
 
 ruas2
-ruas3 = openxlsx::read.xlsx("C:\\Users\\rafae\\OneDrive\\Ãrea de Trabalho\\ruas.xlsx",sheet = "Sheet6")
+ruas3 = openxlsx::read.xlsx("C:\\Users\\belokurowsr\\Downloads\\ruas.xlsx",sheet = "Sheet6")
 ruas3 %>% View()
 ruas4 = ruas3 %>% mutate(sexoFinal2 = case_when(ajustar=="Nada"~NA_character_,
                            ajustar=="Male"~"Male",
                            ajustar=="Female"~"Female",
-                           is.na(ajustar)~sexoFinal)) 
+                           is.na(ajustar)~sexoFinal))
 
-# %>% group_by(is.na(sexoFinal2)) %>% 
-#   count(sexoFinal2) %>% 
+# %>% group_by(is.na(sexoFinal2)) %>%
+#   count(sexoFinal2) %>%
 #   mutate(prop=n/sum(n))
+str(ruas4)
+str(streets$osm_lines)
+class(ruas4)
 
-streets$osm_lines = streets$osm_lines %>% 
-  left_join(ruas4 %>% select(rua,sexoFinal2),by=c("name"="rua"))
+streets$osm_lines %>%
+  mutate(name = iconv(name,"UTF-8","latin1")) %>%
+  left_join(ruas4 %>%   select(rua,clean,sexoFinal2),by=c("name"="rua")) %>%
+  select(name,clean,sexoFinal2) %>% distinct(name,clean,sexoFinal2) %>% filter(is.na(clean))
+
+ruas %>% left_join(ruas4,by=c("rua","nome","clean"))
+
+ruas %>% filter(!clean %in% ruas4$clean)
 
 
 blankbg <-theme(axis.line=element_blank(),
@@ -134,40 +152,40 @@ leaflet() %>%
   addProviderTiles("CartoDB.DarkMatter", group="Dark") %>%
   #addProviderTiles("CartoDB.VoyagerLabelsUnder", group="background 1") %>%
   addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
-  addProviderTiles(provider = "CartoDB.Positron",group="Carto DB") %>% 
+  addProviderTiles(provider = "CartoDB.Positron",group="Carto DB") %>%
   #addTiles() %>%
   leaflet::addPolylines(
-    data = streets$osm_lines %>% 
+    data = streets$osm_lines %>%
       filter(is.na(sexoFinal2)),
     color = "grey",
     smoothFactor = 0.8,
     weight = 1,
     group="Outros",
-    label=streets$osm_lines %>% 
+    label=streets$osm_lines %>%
       filter(is.na(sexoFinal2)) %>% pull(name)
-  ) %>% 
+  ) %>%
   leaflet::addPolylines(
-    data = streets$osm_lines %>% 
+    data = streets$osm_lines %>%
       filter(sexoFinal2 == "Male"),
     color = "blue",
     smoothFactor = 0.8,
     weight = 1.8,
     group="Homens",
-    label = streets$osm_lines %>% 
+    label = streets$osm_lines %>%
       filter(sexoFinal2 == "Male") %>% pull(name)
-    
-  ) %>% 
+
+  ) %>%
   leaflet::addPolylines(
-    data = streets$osm_lines %>% 
+    data = streets$osm_lines %>%
       filter(sexoFinal2 == "Female"),
     color = "#FF5F15",
     smoothFactor = 0.8,
     weight = 2.5,
     group="Mulheres",
-    label = streets$osm_lines %>% 
+    label = streets$osm_lines %>%
       filter(sexoFinal2 == "Female") %>% pull(name)
-    
-  ) %>% 
+
+  ) %>%
   addLayersControl( baseGroups = c("Dark","Toner Lite","Carto DB"),
-                    overlayGroups = c("Homens","Mulheres","Outros"), 
+                    overlayGroups = c("Homens","Mulheres","Outros"),
                    options = layersControlOptions(collapsed = FALSE))
